611ce060990443577d02d6227d50b79f479fe8ad
McGeenie <89535011+McGeenie@users.noreply.github.com>
2023-08-14 17:53:24 -0400

    US04 Completed

--- src/test/java/com/library/steps/US05_StepDefs.java
+++ src/test/java/com/library/steps/US05_StepDefs.java
@@ -1,17 +1,27 @@
 package com.library.steps;
 
+import com.library.utility.DB_Util;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class US05_StepDefs {
 
     @When("I execute query to find most popular book genre")
     public void i_execute_query_to_find_most_popular_book_genre() {
+        String query = "select name from book_categories\n" +
+                "            where id = (select book_category_id from books\n" +
+                "            where id = (Select book_id from book_borrow\n" +
+                "            group by book_id\n" +
+                "            order by count(*)desc\n" +
+                "            limit 1))";
 
+        DB_Util.runQuery(query);
     }
     @Then("verify {string} is the most popular book genre.")
-    public void verify_is_the_most_popular_book_genre(String string) {
-
+    public void verify_is_the_most_popular_book_genre(String expectedCategory) {
+        String actualCategory = DB_Util.getFirstRowFirstColumn();
+        Assert.assertEquals(expectedCategory,actualCategory);
     }
 
 }
--- target/test-classes/features/us05.feature
+++ target/test-classes/features/us05.feature
@@ -1,4 +1,4 @@
-@US05
+@US05 @db @ui
 Feature: As a data consumer, I want to know genre of books are being borrowed the most
 
   Scenario: verify the the common book genre that’s being borrowed
